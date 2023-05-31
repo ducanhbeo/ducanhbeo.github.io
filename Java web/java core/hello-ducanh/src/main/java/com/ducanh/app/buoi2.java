6118de4233c85771a580578330105c4ec2a59550
DucEng <112877906+ducanhbeo@users.noreply.github.com>
2023-05-31 19:19:01 +0700

    them file

--- Java web/java core/hello-ducanh/src/main/java/com/ducanh/app/buoi2.java
+++ Java web/java core/hello-ducanh/src/main/java/com/ducanh/app/buoi2.java
@@ -2,6 +2,8 @@ package com.ducanh.app;
 
 public class buoi2 {
     public static void main(String[] args) {
-        System.out.println("hello fucking world");
+        int a;
+        a=10*5;
+        System.out.println(a);
     }
 }
