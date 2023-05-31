08093bd45abea0becff6572744508dae92867e54
DucEng <112877906+ducanhbeo@users.noreply.github.com>
2023-05-31 18:40:01 +0700

    them file

---
+++ Java web/java core/hello-ducanh/src/main/java/com/ducanh/app/helloWorldApp.java
@@ -0,0 +1,23 @@
+package com.ducanh.app;
+
+public class helloWorldApp {
+    int name;
+    long age;
+
+    boolean sex;
+
+    public static void main(String[] args) {
+        ham1();
+    }
+
+
+    public static void ham1() {
+
+    }
+
+    public void ham2() {
+
+    }
+
+
+}
