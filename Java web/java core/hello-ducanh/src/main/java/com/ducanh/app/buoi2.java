4609402fe277b3e372f6d92f013d4a7c6a624ef2
DucEng <112877906+ducanhbeo@users.noreply.github.com>
2023-05-31 19:37:05 +0700

    yh

--- Java web/java core/hello-ducanh/src/main/java/com/ducanh/app/buoi2.java
+++ Java web/java core/hello-ducanh/src/main/java/com/ducanh/app/buoi2.java
@@ -2,8 +2,10 @@ package com.ducanh.app;
 
 public class buoi2 {
     public static void main(String[] args) {
-        int a;
+        int a,b;
         a=10*5;
+        b=10*2;
         System.out.println(a);
+        System.out.println(b);
     }
 }
